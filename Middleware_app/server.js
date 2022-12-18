3128806005d917e88c2bafa2bda2348ef54fd30d
roshan786143 <roshanbabu.kng@gmail.com>
2022-12-18 08:41:03 +0530

    basic middleware files app

---
+++ Middleware_app/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const date = new Date();
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use((req,res,next)=>{
+    console.log(`Request date : ${date}`);
+    // res.send('Sunny leone...')
+    next()
+})
+
+app.use((req,res,next)=>{
+    const filepath = path.join(__dirname,"static",req.url);
+    fs.stat(filepath,(err,file_info)=>{
+        if(err){
+            next();
+            return;
+        }
+        if(file_info.isFile()){
+            console.log(`${file_info}`);
+            res.sendFile(filepath);
+        }else{
+            next();
+        }
+    })
+})
+
+app.use((req,res)=>{
+    res.status(404);
+    res.send("<h2>404-File Not Found!</h2>");
+})
+
+app.listen(port,()=>console.log('hey,the server has started'));
